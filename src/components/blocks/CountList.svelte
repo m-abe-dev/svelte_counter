0db5b8f3e12593f2cc165e3bac48ed76bb447f5a
Abe Masashi <uuu.theman.jp@gmail.com>
2022-04-15 17:26:02 +0900

    complete uiux

--- src/components/blocks/CountList.svelte
+++ src/components/blocks/CountList.svelte
@@ -5,23 +5,63 @@
   import Add from '../atoms/Add.svelte';
 </script>
 
-<div class="wrap">
-  <Input />
-  <span class="count">{$count < 0 ? 0 : $count}</span>
-  <Button on:click={count.increment} button={'+'} />
-  <Button on:click={count.decrement} button={'-'} />
-  <Button on:click={count.reset} button={'0'} />
+<div class="color">
+  <span class="close" />
+  <div class="wrap">
+    <Input />
+    <span class="count">{$count < 0 ? 0 : $count}</span>
+    <Button on:click={count.increment} button={'+'} />
+    <Button on:click={count.decrement} button={'-'} />
+    <Button on:click={count.reset} button={'0'} />
+  </div>
+  <Add text={'ADD NEW COUNTER'} />
 </div>
-<Add text={'ADD NEW COUNTER'} />
 
 <style>
+  .color {
+    background-color: aliceblue;
+    padding: 10px 10px 30px 10px;
+    margin: 10px 400px;
+    border-radius: 10px;
+  }
+
+  .close {
+    display: block;
+    position: relative;
+    width: 30px;
+    height: 30px;
+    opacity: 0.6;
+  }
+
+  .close:hover {
+    opacity: 0.3;
+  }
+
+  .close::before,
+  .close::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    right: -1750%;
+    width: 2px;
+    height: 20px;
+    background: #333;
+  }
+
+  .close::before {
+    transform: translate(-50%, -50%) rotate(45deg);
+  }
+
+  .close::after {
+    transform: translate(-50%, -50%) rotate(-45deg);
+  }
+
   .wrap {
     display: flex;
     justify-content: center;
     align-items: center;
     border: 10px;
     box-shadow: inset;
-    padding: 50;
     margin-bottom: 20px;
   }
   .count {
--- src/components/pages/Count.svelte
+++ src/components/pages/Count.svelte
@@ -3,15 +3,21 @@
 </script>
 
 <h1>Svelte Counter</h1>
-<p class="highlight">Title List: new</p>
-<p class="highlight">Total : 2</p>
+<div class="highlight"><span>Title List : </span> new</div>
+<div class="highlight"><span>Total :</span> 2</div>
 <CountList />
 
 <style>
   h1 {
     font-size: 80px;
   }
+
+  span {
+    font-weight: bold;
+  }
   .highlight {
     color: red;
+    font-size: 20px;
+    margin-bottom: 10px;
   }
 </style>
